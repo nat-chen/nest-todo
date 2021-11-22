911aa9aa70882288d7ad804ff2c9183f3e7811f7
Nat Chen <chen_natalie@outlook.com>
2021-11-22 15:28:15 +0800

    成功调通 fidnAll 单元测试

--- server/src/todo/todo.service.ts
+++ server/src/todo/todo.service.ts
@@ -1,30 +1,31 @@
-import { PrismaService } from './../../prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
 
 @Injectable()
 export class TodoService {
   constructor(private prisma: PrismaService) {}
 
-  create(createTodoDto: CreateTodoDto) {
+  create(createTodoDto: CreateTodoDto): Promise<Todo> {
     const { title, description } = createTodoDto;
     return this.prisma.todo.create({
       data: { title, description, status: 1 }
     });
   }
 
-  findAll() {
+  findAll(): Promise<Todo[]> {
     return this.prisma.todo.findMany();
   }
 
-  findOne(id: number) {
+  findOne(id: number): Promise<Todo> {
     return this.prisma.todo.findUnique({
       where: { id }
     });
   }
 
-  update(id: number, updateTodoDto: UpdateTodoDto) {
+  update(id: number, updateTodoDto: UpdateTodoDto): Promise<Todo> {
     const { title, description, status } = updateTodoDto;
     return this.prisma.todo.update({
       data: { title, description, status },
@@ -32,7 +33,7 @@ export class TodoService {
     });
   }
 
-  remove(id: number) {
+  remove(id: number): Promise<Todo> {
     return this.prisma.todo.delete({
       where: { id }
     })
