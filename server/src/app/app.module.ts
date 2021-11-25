270b452f7d4ab494c118887f0eea1234787aadf9
Nat Chen <chen_natalie@outlook.com>
2021-11-25 15:19:37 +0800

    feat: 添加文件上传接口

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TodoModule } from '../todo/todo.module';
 import { UserModule } from '../user/user.module';
 import { AuthModule } from './../auth/auth.module';
+import { UploadModule } from './../upload/upload.module';
 
 @Module({
-  imports: [TodoModule, UserModule, AuthModule],
+  imports: [TodoModule, UserModule, AuthModule, UploadModule],
   controllers: [AppController],
   providers: [AppService],
 })
